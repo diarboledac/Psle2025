4bc2a680632bfd0c5fa295ee05befc6115a9d813
Diego <diarboledac@unal.edu.co>
2025-07-23 13:15:20 -0500

    Agregamos el poder re-escanear la red y ver el fabricante

--- Network-scaner/web/app.py
+++ Network-scaner/web/app.py
@@ -1,22 +1,11 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template, request
 from .scanner import scan_network
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Servidor Flask funcionando correctamente desde la Lychee.'
-
-@app.route('/scan')
+@app.route("/", methods=["GET", "POST"])
 def scan():
-    devices = scan_network()
-    html = """
-    <h2>Dispositivos conectados a la red</h2>
-    <table border="1">
-        <tr><th>IP</th><th>MAC</th></tr>
-        {% for d in devices %}
-        <tr><td>{{ d.ip }}</td><td>{{ d.mac }}</td></tr>
-        {% endfor %}
-    </table>
-    """
-    return render_template_string(html, devices=devices)
+    devices = []
+    if request.method == "POST":
+        devices = scan_network()
+    return render_template("index.html", devices=devices)
